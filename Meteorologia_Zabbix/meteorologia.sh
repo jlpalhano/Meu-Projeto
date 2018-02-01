70a7377829040b111963d261054d27036c1e8253
root <root@debian.dark.ness>
2018-02-01 11:31:34 -0200

    Indentação

--- Meteorologia_Zabbix/meteorologia.sh
+++ Meteorologia_Zabbix/meteorologia.sh
@@ -14,12 +14,11 @@ DIR_TEMP=/etc/zabbix/prev_tempo
 
 for cidade in `echo $cidade bento-goncalves-rs canela-rs vacaria-rs santa-cruz-do-sul-rs cachoeira-do-sul-rs gramado-rs taquara-rs porto-alegre-rs novo-hamburgo-rs sao-leopoldo-rs`
 do
-> $DIR_TEMP/$cidade.txt
-
-lynx -dump http://www.sensacaotermica.com.br/$cidade | grep  "Velocidade do Vento:" | awk -F: '{print $2}' | tr -d km/h | tr -d " "  >> $DIR_TEMP/$cidade.txt
-lynx -dump http://www.sensacaotermica.com.br/$cidade | grep "Temperatura:" | awk -F: '{print $2}' | tr -d º | tr -d " "  >> $DIR_TEMP/$cidade.txt
-lynx -dump http://www.sensacaotermica.com.br/$cidade | grep "Sensação Térmica:"  | awk -F: '{print $2}' | tr -d º | tr -d " "  >> $DIR_TEMP/$cidade.txt
-lynx -dump http://www.sensacaotermica.com.br/$cidade | grep  "Clima Atual:"  | awk -F: '{print $2}'  | sed 's/^ \+//'  >> $DIR_TEMP/$cidade.txt
+	> $DIR_TEMP/$cidade.txt
+	lynx -dump http://www.sensacaotermica.com.br/$cidade | grep  "Velocidade do Vento:" | awk -F: '{print $2}' | tr -d km/h | tr -d " "  >> $DIR_TEMP/$cidade.txt
+	lynx -dump http://www.sensacaotermica.com.br/$cidade | grep "Temperatura:" | awk -F: '{print $2}' | tr -d º | tr -d " "  >> $DIR_TEMP/$cidade.txt
+	lynx -dump http://www.sensacaotermica.com.br/$cidade | grep "Sensação Térmica:"  | awk -F: '{print $2}' | tr -d º | tr -d " "  >> $DIR_TEMP/$cidade.txt
+	lynx -dump http://www.sensacaotermica.com.br/$cidade | grep  "Clima Atual:"  | awk -F: '{print $2}'  | sed 's/^ \+//'  >> $DIR_TEMP/$cidade.txt
 
 done
 
